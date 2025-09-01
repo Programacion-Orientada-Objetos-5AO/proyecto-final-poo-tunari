2cf3b93677f1250508373215a03ad65e98feb3af
facubora <fborassi@alumno.huergo.edu.ar>
2025-09-01 19:02:45 -0300

    Creación del java project

---
+++ tunari/src/test/java/ar/edu/huergo/aguilar/borassi/tunari/TunariApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.huergo.aguilar.borassi.tunari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
